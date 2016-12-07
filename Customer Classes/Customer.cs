2ffc514c90ab4ecda17a97fbc65ced9da27f7195
daboss9876 <daboss9876@gmail.com>
2016-12-07 13:37:02 -0500

    Add files via upload

--- Customer Classes/Customer.cs
+++ Customer Classes/Customer.cs
@@ -23,7 +23,5 @@ namespace BizWorks
         }
 
         //Properties
-        
-        
     }
 }
--- Customer Classes/CustomerCollection.cs
+++ Customer Classes/CustomerCollection.cs
@@ -1,1 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace BizWorks
+{
+    public class CustomerCollection
+    {
+        private static List<Customer> CustomerList = new List<Customer>();
+
+        public static void AddCustomer(int profileTypeID, string user, string pass,
+            string workNum, string faxNum, string email, DateTime createdOn, string createdBy,
+            DateTime lastUpdatedOn, string lastUpdatedBy, string notes)
+        {
+            CustomerList.Add(new Customer(profileTypeID,
+                user, pass, workNum, faxNum, email, createdOn,
+                createdBy, lastUpdatedOn, lastUpdatedBy, notes));
+        }
+    }
+
+    }
+}
