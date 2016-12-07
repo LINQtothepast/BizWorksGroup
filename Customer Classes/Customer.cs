810c893402c9cdd9512b2407dd406a5d6c83e357
daboss9876 <daboss9876@gmail.com>
2016-12-07 13:19:44 -0500

    Add files via upload

---
+++ Customer Classes/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizWorks
+{
+    class Customer : UserInfo
+    {
+
+        //Attributes
+
+        //Constructor
+        public Customer(int profileTypeID, string user, string pass,
+            string workNum, string faxNum, string email, DateTime createdOn, string createdBy,
+            DateTime lastUpdatedOn, string lastUpdatedBy, string notes)
+            : base (profileTypeID, user, pass, workNum, faxNum, email,
+                    createdOn, createdBy, lastUpdatedOn, lastUpdatedBy,
+                    notes)
+        {
+
+        }
+
+        //Properties
+    }
+}
