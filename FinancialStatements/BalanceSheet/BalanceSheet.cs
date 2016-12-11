4afb34e15d0c2f3cdf9f842c1ef016a20304a730
Hunter Johnson <hhjohnso@indiana.edu>
2016-12-11 18:43:08 -0500

    Update BalanceSheet.cs

--- FinancialStatements/BalanceSheet/BalanceSheet.cs
+++ FinancialStatements/BalanceSheet/BalanceSheet.cs
@@ -8,12 +8,35 @@ namespace BizWorks
 {
     public class BalanceSheet 
     {
-        private Date startDate;
-        private Date endDate;
+        private DateTime startDate;
+        private DateTime endDate;
+        private float assets;
+        private float debts;
    
         //constructor
-        public BalanceSheet(Date start, Date end)
+        public BalanceSheet(DateTime start, DateTime end)
         { 
             startDate = start;
-            endDate = end
+            endDate = end;
+            
+            List<Transaction> trans = TransactionCollection.TransactionList;
+            
+            for(int i=0; i < trans.Count; i++)
+            {
+                if(trans[i].TransactionDate > start && trans[i].TransactionDate < end)
+                {
+                    if(trans[i].isCredit == true)
+                    {
+                        assets += trans[i].TransactionAmount;
+                    }
+                    else
+                    {
+                        debts += trans[i].TransactionAmount;
+                    }
+                }
+            }
+        }
+        
+                        
+             
         }
